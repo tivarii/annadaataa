a10cfcc05029b6cba1da4a7cc1ccf2c8df8ba380
tivarii <adarshtiwari3576@gmail.com>
2024-05-17 15:11:36 +0530

    first commit

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,6 +1,5 @@
 import { About } from "@/components/About";
 import { Navbar } from "@/components/Navbar";
-import { Import } from "lucide-react";
 import { Hero } from "@/components/Hero";
 import { HowItWorks } from "@/components/HowItWorks";
 export const LandingPage=()=>{
